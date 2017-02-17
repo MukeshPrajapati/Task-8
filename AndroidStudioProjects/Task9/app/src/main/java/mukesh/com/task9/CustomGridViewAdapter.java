46d51e17087cb6bc9688f2c1cd27312661cbf0b6
MukeshPrajapati <mukesh.p.rockers@gmail.com>
2017-02-17 18:57:30 +0530

    Implemented Application

--- AndroidStudioProjects/Task9/app/src/main/java/mukesh/com/task9/CustomGridViewAdapter.java
+++ AndroidStudioProjects/Task9/app/src/main/java/mukesh/com/task9/CustomGridViewAdapter.java
@@ -1,8 +1,10 @@
 package mukesh.com.task9;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,10 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class CustomGridViewAdapter extends ArrayAdapter<CategoryPost> {
-
-    Context context;
-    int layoutResourceId;
-    ArrayList<CategoryPost> data = new ArrayList<CategoryPost>();
+    private Context context;
+    private int layoutResourceId;
+    private ArrayList<CategoryPost> data = new ArrayList<CategoryPost>();
+    private Fragment getquoteFrag;
 
     public CustomGridViewAdapter(Context context, int resource, ArrayList<CategoryPost> objects) {
         super(context, resource, objects);
@@ -22,16 +24,14 @@ public class CustomGridViewAdapter extends ArrayAdapter<CategoryPost> {
         this.context = context;
         this.data = objects;
     }
-
-
     static class ViewHolder
     {
         TextView success, message, id, name;
     }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         final ViewHolder holder;
-
         if (convertView == null){
             holder = new ViewHolder();
             convertView = LayoutInflater.from(context).inflate(layoutResourceId, parent, false);
@@ -39,21 +39,26 @@ public class CustomGridViewAdapter extends ArrayAdapter<CategoryPost> {
             holder.name = (TextView)convertView.findViewById(R.id.tv_name_id);
             convertView.setTag(holder);
         }
-
         else {
             holder = (ViewHolder) convertView.getTag();
         }
-
         final CategoryPost post = data.get(position);
         holder.name.setText(post.getName());
-        /*holder.name.setOnClickListener(new View.OnClickListener() {
+        holder.name.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), GetQuotes.class);
-                intent.putExtra("pos", post.getId());
-                context.startActivity(intent);
+                Bundle bundle = new Bundle();
+                getquoteFrag = new GetQuoteFragment();
+
+                FragmentManager quote_fm = ((MainActivity)context).getSupportFragmentManager();
+                FragmentTransaction quote_ft = quote_fm.beginTransaction();
+                bundle.putInt("pos", post.getId());
+                getquoteFrag.setArguments(bundle);
+                quote_ft.replace(R.id.ll_id, getquoteFrag);
+                quote_ft.addToBackStack(null);
+                quote_ft.commit();
             }
-        });*/
+        });
         return convertView;
     }
 }
